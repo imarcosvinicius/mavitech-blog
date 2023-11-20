540df1b5a7d2082a91ee8033582fe56a25ba0096
Marcos Vinícius Nunes <imarcosvinicius@outlook.com>
2023-11-20 17:35:43 -0300

    implemented the listing of posts in the posts route

---
+++ src/main/java/space/mavitech/mavitech/controller/BlogController.java
@@ -0,0 +1,28 @@
+package space.mavitech.mavitech.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import space.mavitech.mavitech.model.Post;
+import space.mavitech.mavitech.service.BlogService;
+
+import java.util.List;
+
+@Controller
+public class BlogController {
+
+    @Autowired
+    BlogService blogService;
+
+    @RequestMapping(value = "/posts", method = RequestMethod.GET)
+    public ModelAndView getPosts() {
+        ModelAndView mv = new ModelAndView("posts");
+        List<Post> posts = blogService.findAll();
+        mv.addObject("posts", posts);
+        return mv;
+    }
+
+
+}
